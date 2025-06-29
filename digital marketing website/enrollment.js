742e3608963e88cc04ebf6edcd3ad404f1ebd93d
Joma-15 <adelzarajhonmarcel@gmail.com>
2025-06-29 18:14:35 +0800

    still figuring out how to connect excel db to my js code

--- digital marketing website/enrollment.js
+++ digital marketing website/enrollment.js
@@ -14,3 +14,66 @@ function PlanChoose(event){
              
     }
 }
+
+
+  function openClientModal() {
+    const groupModal = bootstrap.Modal.getInstance(document.getElementById('group-modal'));
+    groupModal.hide();
+
+    const clientDetailsModal = new bootstrap.Modal(document.getElementById('clientDetailsModal'));
+    clientDetailsModal.show();
+  }
+
+
+//filter send data
+function filterData(){
+  // const scriptUrl = "https://script.google.com/macros/s/AKfycbyWmGFKgo2e1luqhr704OIlJtwtRHpWBXKxWw4RO-5vWBD46_OkPUsEUpc926jhlh5U/exec"; 
+
+  document.getElementById('clientDetailsModal').addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const lastName = document.getElementById('LastName').value.trim(); 
+    const firstName = document.getElementById('FirstName').value.trim(); 
+    const middlename = document.getElementById('MiddleName').value.trim();
+
+    //data to be send in the excel sheet
+    const name = `${lastName} ${firstName} ${middlename}`; 
+    const birthDate = document.getElementById('bday').value; 
+    const address = document.getElementById('address').value; 
+    const email = document.getElementById('email').value;
+    
+    //use queryselector for the radio button 
+    const gender = document.querySelector('input[name="gender"]:checked')?.value;
+    const civiStatus = document.getElementById('civil-status').value;
+
+    const data = {
+      name, 
+      birthDate, 
+      address, 
+      email, 
+      gender, 
+      civiStatus
+    }; 
+
+    sendDataToDB(data);
+    
+  });
+}
+
+//send data to the database using async await
+async function sendDataToDB(data){
+  const scriptUrl =  "https://script.google.com/macros/s/AKfycbyWmGFKgo2e1luqhr704OIlJtwtRHpWBXKxWw4RO-5vWBD46_OkPUsEUpc926jhlh5U/exec";
+  
+  try {
+    const response = await fetch(scriptUrl, {
+      method: 'POST', 
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(data)
+    }); 
+    const result = await response.text();
+    alert('succes' + result);
+
+  } catch (error) {
+    console.error('Submission error', error);
+  }
+}
