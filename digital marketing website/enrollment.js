26431d57478b8a7f558a113bce999bbf52c2395d
Joma-15 <adelzarajhonmarcel@gmail.com>
2025-06-30 16:16:59 +0800

    fixing the excel sheet database

--- digital marketing website/enrollment.js
+++ digital marketing website/enrollment.js
@@ -22,58 +22,62 @@ function PlanChoose(event){
 
     const clientDetailsModal = new bootstrap.Modal(document.getElementById('clientDetailsModal'));
     clientDetailsModal.show();
-  }
-
-
-//filter send data
-function filterData(){
-  // const scriptUrl = "https://script.google.com/macros/s/AKfycbyWmGFKgo2e1luqhr704OIlJtwtRHpWBXKxWw4RO-5vWBD46_OkPUsEUpc926jhlh5U/exec"; 
-
-  document.getElementById('clientDetailsModal').addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    const lastName = document.getElementById('LastName').value.trim(); 
-    const firstName = document.getElementById('FirstName').value.trim(); 
-    const middlename = document.getElementById('MiddleName').value.trim();
-
-    //data to be send in the excel sheet
-    const name = `${lastName} ${firstName} ${middlename}`; 
-    const birthDate = document.getElementById('bday').value; 
-    const address = document.getElementById('address').value; 
-    const email = document.getElementById('email').value;
-    
-    //use queryselector for the radio button 
-    const gender = document.querySelector('input[name="gender"]:checked')?.value;
-    const civiStatus = document.getElementById('civil-status').value;
-
-    const data = {
-      name, 
-      birthDate, 
-      address, 
-      email, 
-      gender, 
-      civiStatus
-    }; 
-
-    sendDataToDB(data);
-    
-  });
-}
-
-//send data to the database using async await
-async function sendDataToDB(data){
-  const scriptUrl =  "https://script.google.com/macros/s/AKfycbyWmGFKgo2e1luqhr704OIlJtwtRHpWBXKxWw4RO-5vWBD46_OkPUsEUpc926jhlh5U/exec";
   
-  try {
-    const response = await fetch(scriptUrl, {
-      method: 'POST', 
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify(data)
-    }); 
-    const result = await response.text();
-    alert('succes' + result);
-
-  } catch (error) {
-    console.error('Submission error', error);
   }
-}
+
+//sending data from the form to the excel sheet 
+async function sendData(){
+  console.log('send data called')
+  function filteredData(){//filter data first before sending 
+    const lastName = document.getElementById('LastName'); 
+    const firstName = document.getElementById('FirstName');
+    const middleName = document.getElementById('MiddleName');
+
+    if (lastName && firstName && middleName) {//if the element exist in the dom
+      const fullName = `${lastName.value} ${firstName.value} ${middleName.value}`;
+      const birthDate = document.getElementById('bday')?.value.trim(); 
+      const address = document.getElementById('address')?.value.trim(); 
+      const email = document.getElementById('email')?.value.trim();
+      const gender = document.querySelector('input[name="gender"]:checked')?.value;
+      const civilStatus = document.getElementById('civil-status')?.value;
+
+      if(fullName && birthDate && address && email && gender && civilStatus){
+        const data = {
+          fullName, 
+          birthDate, 
+          address, 
+          email, 
+          gender, 
+          civilStatus
+        };
+        return data;
+
+      }else{
+        alert('please complete this all field');
+        return null;
+      }
+     }
+    }
+
+    const data = filteredData(); 
+    if(!data) return;
+
+    const scriptUrl = "https://script.google.com/macros/s/AKfycbwWPN0ohuw17pO-zC2DTMwsGeZRf4G-nOA4YlA61EBxKxM291Ug7N7MBeF3XhGDlxJTRg/exec";
+    try {
+      const response = await fetch(scriptUrl, {
+        method: 'POST', 
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        }, 
+        body: JSON.stringify(filteredData()),
+      });
+
+      if(!response.ok){
+        throw new error('Server responded with status ' + response.status);
+      }
+      console.log('data has been sent successfully ' + response);
+    } catch (error) {
+      console.error('Error sending data: ', error)
+    }
+  }
+
