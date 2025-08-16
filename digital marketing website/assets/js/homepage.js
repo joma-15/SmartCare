6b1b69e20df2faa51a7e05a8a96c2b365036571d
Joma-15 <adelzarajhonmarcel@gmail.com>
2025-08-16 11:04:31 +0800

    change the homepage link

--- digital marketing website/assets/js/homepage.js
+++ digital marketing website/assets/js/homepage.js
@@ -12,10 +12,21 @@ handleDrawer();
 
 function handlePreloader(){
     // Preloader fade-out after 3 seconds
-    window.addEventListener("load", function(){
-      setTimeout(function(){
-        document.getElementById("preloader").classList.add("fade-out");
-      }, 1000);
-    });
+    // window.addEventListener("load", function(){
+    //   setTimeout(function(){
+    //     document.getElementById("preloader").classList.add("fade-out");
+    //   }, 1000);
+    // });
+
+    const preloader = document.getElementById("preloader"); 
+
+    if (!sessionStorage.getItem("preloaderShown")) {
+      setTimeout(() => {
+        preloader.classList.add("fade-out"); 
+        sessionStorage.setItem("preloaderShown", "true"); 
+      }, 3000);
+    }else{
+      preloader.style.display = "none"; //dont show anything in the screen
+    }
 }
 handlePreloader();
